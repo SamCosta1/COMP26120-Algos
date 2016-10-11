cc7f4e0108969d46c56d87d80cbeaab39ee92aee
Sam <sam.dacosta@student.manchester.ac.uk>
2016-10-11 10:38:08 +0100

    Finish part2 B

--- ex2/part2.c
+++ ex2/part2.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 int main(int argc, char **argv)
 {
     int numToUpperCase = 0,
         numToLowerCase = 0,
         chr;
+
+    char *filename = "input.txt";
     printf("\n");
 
+    // ---- Taken from SalaryAnalysis.c --- //
+
+    // A stream for getting data from the user.
+    FILE *stream= fopen(filename, "r");
+    if (!stream) {
+        fprintf(stderr, "can't open %s for reading\n", filename);
+        exit(-1);
+    }
+
+    // ------------------------------- //
+
     // Loop through input until terminated
-    while((chr = getchar()) != EOF) {
+    while ((chr = fgetc(stream)) != EOF) {
         if (isupper(chr)) {
             numToUpperCase++;
             putchar(tolower(chr));
@@ -25,5 +39,6 @@ int main(int argc, char **argv)
             numToUpperCase,
             numToLowerCase);
 
+    fclose(stream);
     return 0;
 }
