5187e4698d90f401ffa4dcf2976ed9609c15c8ba
Sam <sam.dacosta@student.manchester.ac.uk>
2016-10-21 19:53:34 +0100

    Finish part 4

--- ex4/arrays.c
+++ ex4/arrays.c
@@ -11,22 +11,22 @@ struct person {
 	int age;
 };
 
-static void insert(struct person **people, char *name, int age) {
-  static int nextfreeplace = 0;
+static void insert(struct person **people, char *name, int age, int* nextInsert)
+{
+  people[*nextInsert] = (struct person *)malloc(sizeof(struct person));
 
-  people[nextfreeplace] = (struct person *)malloc(sizeof(struct person));
-
-  people[nextfreeplace]->name = name;
-  people[nextfreeplace++]->age = age;
+  people[*nextInsert]->name = name;
+  people[(*nextInsert)++]->age = age;
 }
 
 int main(int argc, char **argv) {
 
   struct person *people[HOW_MANY];
+  int nextInsert = 0;
   int i;
 
   for (i = 0; i < HOW_MANY; i++)  {
-    insert (people, names[i], ages[i]);
+    insert (people, names[i], ages[i], &nextInsert);
   }
 
   for (i = 0; i < HOW_MANY; i++)  {
