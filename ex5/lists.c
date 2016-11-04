cff6c07cc60e130558ee32fc31d953629dc6fc9a
Sam <sam.dacosta@student.manchester.ac.uk>
2016-11-04 14:07:29 +0000

    Finishe part 3

--- ex5/lists.c
+++ ex5/lists.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define HOW_MANY 7
 char *names[HOW_MANY]= {"Simon", "Suzie", "Alfred", "Chip", "John", "Tim",
@@ -14,10 +15,56 @@ struct person {
 
 struct person * insert_start(struct person *people, char *name, int age) {
 	struct person *current = (struct person *)malloc(sizeof(struct person));
+	
+	if (current == NULL) {
+		printf("Something went wrong with memory");
+		exit(-1);
+	}
 	current->name = name;
-    current->age = age;
+  current->age = age;
 	current->next = people;
 	return current;
+}
+
+int personCompare(struct person *person1, struct person *person2) {
+
+	return strcmp(person1->name, person2->name);
+}
+
+struct person * insert_sorted(struct person *people, char *name, int age) {
+	struct person *current = (struct person *)malloc(sizeof(struct person));
+
+	if (current == NULL) {
+		printf("Something went wrong with memory");
+		exit(-1);
+	}
+
+	current->name = name;
+  current->age = age;
+
+	if (people == NULL || personCompare(people, current) > 0) {
+		current->next = people;
+		return current;
+	} else {
+	  
+		struct person *nextPerson = people;
+		while (nextPerson->next != NULL && personCompare(nextPerson->next,current) < 0) {
+		  nextPerson = nextPerson->next;
+		}
+		
+		if (nextPerson->next == NULL) {
+		  nextPerson->next = current;
+		} else {
+		  // The current list is,  nextPerson ---> nextPerson.next
+		  // we need               nextPerson ---> current ---> nxtPerson.next
+		  // ie current needs to be insterted in the middle
+		  struct person *tempPerson = nextPerson->next;
+		  nextPerson->next = current;
+		  current->next = tempPerson;		
+		}
+		
+		return people;
+	}
 }
 
 struct person * insert_end(struct person *people, char *name, int age) {
@@ -52,7 +99,7 @@ int main(int argc, char **argv) {
   int i;
 
   for (i = 0; i < HOW_MANY; i++)  {
-    people = insert_end(people, names[i], ages[i]);
+    people = insert_sorted(people, names[i], ages[i]);
   }
 
   nextPerson = people;
