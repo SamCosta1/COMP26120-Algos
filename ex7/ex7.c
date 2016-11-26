5561fe79774bc3918632b947d20ffc42e4202f03
Sam <sam.dacosta@student.manchester.ac.uk>
2016-11-26 16:08:15 +0000

    Part 2

--- ex7/ex7.c
+++ ex7/ex7.c
@@ -9,6 +9,30 @@ unsigned long hcf(unsigned long a, unsigned long b) {
         return hcf(b, a % b);
 }
 
+unsigned long fme(unsigned long primitiveRoot, unsigned long exponent,
+                  unsigned long prime) {
+    unsigned long d = primitiveRoot;
+    unsigned long e = exponent;
+    unsigned long accumulator = 1;
+
+    while (e != 0) {
+        if ((e % 2) != 0)
+            accumulator = (accumulator * d) % prime;
+        d = (d * d) % prime;
+        e = e / 2;
+    }
+
+    return accumulator;
+}
+
+void testfme() {
+    printf("%lu^%lu mod %lu | %lu\n", 5, 15, 7, fme(5,15,7));
+    printf("%lu^%lu mod %lu | %lu\n", 3, 10, 17, fme(3, 10, 17));
+    printf("%lu^%lu mod %lu | %lu\n", 3, 12, 257, fme(3, 12, 257));
+    printf("%lu^%lu mod %lu | %lu\n", 2, 62, 443, fme(2, 62, 443));
+    printf("%lu^%lu mod %lu | %lu\n", 2, 34, 4093082899, fme(2, 34, 4093082899));
+
+}
 int main(int argc, char **argv) {
-    printf("%d %d | %d\n", 100, 1734128, hcf(100, 1734128));
+    testfme();
 }
