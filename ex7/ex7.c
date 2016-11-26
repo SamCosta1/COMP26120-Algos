fd2d863dec5260252c54d2215c6afbb57567ae9d
Sam <sam.dacosta@student.manchester.ac.uk>
2016-11-26 17:03:08 +0000

    Part 3

--- ex7/ex7.c
+++ ex7/ex7.c
@@ -25,14 +25,33 @@ unsigned long fme(unsigned long primitiveRoot, unsigned long exponent,
     return accumulator;
 }
 
-void testfme() {
-    printf("%lu^%lu mod %lu | %lu\n", 5, 15, 7, fme(5,15,7));
-    printf("%lu^%lu mod %lu | %lu\n", 3, 10, 17, fme(3, 10, 17));
-    printf("%lu^%lu mod %lu | %lu\n", 3, 12, 257, fme(3, 12, 257));
-    printf("%lu^%lu mod %lu | %lu\n", 2, 62, 443, fme(2, 62, 443));
-    printf("%lu^%lu mod %lu | %lu\n", 2, 34, 4093082899, fme(2, 34, 4093082899));
+unsigned long dl(unsigned long y,unsigned long pRoot,unsigned long prime) {
+    unsigned long x = 1;
+    while (fme(pRoot, x, prime) != y) {
+        x++;
+    }
+    return x;
+}
 
+unsigned long imp(unsigned long y, unsigned long p) {
+
+}
+void testfme() {
+    printf("%d^%d mod %d = %lu\n", 5, 15, 7, fme(5,15,7));
+    printf("%d^%d mod %d = %lu\n", 3, 10, 17, fme(3, 10, 17));
+    printf("%d^%d mod %d = %lu\n", 3, 12, 257, fme(3, 12, 257));
+    printf("%d^%d mod %d = %lu\n", 2, 62, 443, fme(2, 62, 443));
+    printf("%d^%d mod %lu = %lu\n", 2, 16000000, 4093082899, fme(2, 16000000, 4093082899));
+}
+
+void testdl() {
+    printf("%d^%lu mod %d = %d\n", 5, dl(6, 5, 7), 7, 6);
+    printf("%d^%lu mod %d = %d\n", 3, dl(8, 3, 17), 17, 8);
+    printf("%d^%lu mod %d = %d\n", 3, dl(222, 3, 257), 257, 222);
+    printf("%d^%lu mod %lu = %lu\n", 2, dl(2952904674, 2, 4093082899), 4093082899,  2952904674);
 }
 int main(int argc, char **argv) {
     testfme();
+    printf("\n");
+    testdl();
 }
